28208a9881feace8115f2154a08a02318cf7d657
Kyle O'Brien <kobrien15@gmail.com>
2020-03-29 09:26:37 -0500

    Finished Assignment 4

--- queries.sql
+++ queries.sql
@@ -1,7 +1,13 @@
 ## Part 1: Test it with SQL
+  `id` int NOT NULL, # PRIMARY KEY
+  `employer` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `skills` varchar(255) DEFAULT NULL,
 
 ## Part 2: Test it with SQL
+select name from employer where location="St. Louis";
 
 ## Part 3: Test it with SQL
-
+drop table job;
 ## Part 4: Test it with SQL
+SELECT s.name, s.description FROM skill s INNER JOIN job_skills js ON s.id=js.skills_id ORDER BY s.name;
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,26 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    private String location;
 
+    @OneToMany(mappedBy="employer")
+    private final List<Job> jobs = new ArrayList<>();
+
+    public Employer() {}
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
